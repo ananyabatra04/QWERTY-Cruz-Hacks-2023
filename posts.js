f3747b6fdb6df0806a7c04f2e99732983e7ed758
ananyabatra <ananyabatra04@gmail.com>
2023-02-04 21:06:14 -0800

    fixed fourth row and create post functionality

--- posts.js
+++ posts.js
@@ -1,6 +1,6 @@
 function addPost() {
     document.getElementById("posts").innerHTML +=
-        "        <div class=\"w3-third w3-container w3-margin-bottom filterDiv petitions\">\n" +
+        "        <div class=\"w3-fourth w3-container w3-margin-bottom filterDiv all\">\n" +
         "            <div class=\"w3-container w3-white\">\n" +
         "                <p><b>\n" +
         "                    Title</b></p>\n" +
